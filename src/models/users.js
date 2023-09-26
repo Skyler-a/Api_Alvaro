116e469224718af1b235d638b679c066fcf25d0a
unknown <skyarthur352@gmail.com>
2023-09-25 23:09:44 -0300

    Initial configs

---
+++ src/models/users.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Users extends Model {
+    }
+    Users.init({
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        },
+        email: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        },
+        cpf: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        }
+    },
+        {
+            sequelize,
+            timestamps: false,
+            modelName: 'Users',
+        });
+
+
+
+
+    return Users;
+};
